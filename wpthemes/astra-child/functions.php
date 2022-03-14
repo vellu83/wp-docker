db5338a5ea7e0a92d9a93d3656377e17205b0650
root <root@ip-172-26-0-168.eu-central-1.compute.internal>
2022-03-14 13:21:16 +0000

    added webp converter and did some changes to nginx conf

--- wpthemes/astra-child/functions.php
+++ wpthemes/astra-child/functions.php
@@ -5,4 +5,6 @@ add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
 function enqueue_parent_styles() {
    wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
 }
+add_filter( 'elementor/frontend/print_google_fonts', '__return_false' );
+
 ?>
